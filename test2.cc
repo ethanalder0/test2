14c3ae8b3ef59bacd9e3319898ba5807345090c4
ethanalder0 <ea445222@ohio.edu>
2023-09-11 19:54:42 -0400

    please work

---
+++ test2.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ethan Alder>
+ *        Date: <9/11/23>
+ * Description: familiarizing myself with github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello world" << endl;
+    return 0;
+}// main
